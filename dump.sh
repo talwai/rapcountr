ec391bab6835f483e5b896249be9e568dccffbf1
Aaditya Talwai <talwai@dartmouth.edu>
2013-08-14 02:41:32 -0400

    approx. string matching

--- dump.sh
+++ dump.sh
@@ -1,3 +1,3 @@
 URLS=$(ruby find_txt.rb) 
-curl $URLS -s| w3m -dump -T text/html >> snoopdogg.txt
-python src/counting.py snoopdogg.txt
+curl $URLS -s| w3m -dump -T text/html >> eminem.txt
+python src/counting.py eminem.txt
